8ccd22cff1edaf4c3adb5031e4031d92749f9f53
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2025-01-04 22:15:51 +0530

    added image filter

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
+from PIL import Image
 
 app = Flask(__name__)
 
-pipe = pipeline("text-classification", model="eliasalbouzidi/distilbert-nsfw-text-classifier")
+text_pipeline = pipeline("text-classification", model="eliasalbouzidi/distilbert-nsfw-text-classifier")
+image_pipeline = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
 
-@app.route("/classify", methods=["POST"])
+@app.route("/classify-text", methods=["POST"])
 def classify_text():
     try:
         data = request.get_json()
         if "text" not in data:
             return jsonify({"error": "Missing 'text' field in request."}), 400
         text = data["text"]
-        result = pipe(text)
+        result = text_pipeline(text)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/classify-image", methods=["POST"])
+def classify_image():
+    try:
+        if 'image' not in request.files:
+            return jsonify({"error": "No image file provided."}), 400
+
+        image_file = request.files["image"]
+        image = Image.open(image_file.stream)
+
+        result = image_pipeline(image)
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
